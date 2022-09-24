6fc1d216f211daba4b604f1986cc3e7540c1555a
Larry Le <lle114@student.hbuhsd.edu>
2022-09-24 14:36:01 -0700

    Finished Main Page Build

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const contentSection = document.querySelectorAll(".contentSection");
 // MAINSETUP ------------------------------------------------------------------------------------------------------------------------- 
 
 const intersection_controls = {
-    rootMargin: "0% 0% -30% 0%"
+    rootMargin: "0% 0% -25% 0%"
 };
 
 const observer = new IntersectionObserver((entries) => {
@@ -25,4 +25,5 @@ const observer = new IntersectionObserver((entries) => {
 }, intersection_controls);
 
 observer.observe(contentSection[0]);
+observer.observe(contentSection[1]);
 observer.observe(contentSection[2]);
