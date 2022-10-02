6901ede3f80a73278d6cfbcf2e1b67629d46b137
Larry Le <lle114@student.hbuhsd.edu>
2022-10-02 16:40:18 -0700

    [Beta] Robots Page

--- js/contacts.js
+++ js/contacts.js
@@ -1,10 +1,26 @@
+// VARIABLES ----------------------------------------------------------------------------------------------------------------
+
 const cyberlions_header_logo = document.querySelector("#cyberlogoImg");
 const cyberlions_header_text = document.querySelector(".cyberlions-nav-text"); /* This is the text that says "CyberLions" */
 
-cyberlions_header_logo?.addEventListener("click", () => {
+const email = document.getElementById("email");
+const map_location = document.getElementById("location");
+
+// MAIN PAGE ----------------------------------------------------------------------------------------------------------------
+
+cyberlions_header_logo?.addEventListener("click", () => { // clicking on the logo will take you to the main page
     window.location.href = "/";
 });
 
 cyberlions_header_text?.addEventListener("click", () => {
     window.location.href = "/";
+});
+
+// clicking on the image icons will take you to the respective external link (ex. email)
+email?.addEventListener("click", () => {
+    window.location.href = "mailto:whslionsrobotics@gmail.com";
+});
+
+map_location?.addEventListener("click", () => {
+    window.open("https://goo.gl/maps/rhpfRFX1a54xiWTq5");
 });
