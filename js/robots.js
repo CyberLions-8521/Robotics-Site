8793d44bc549da0f136759858a3293a1fdcbb670
Larry Le <lle114@student.hbuhsd.edu>
2022-10-09 12:53:22 -0700

    Media Queries Mobile Support

--- js/robots.js
+++ js/robots.js
@@ -1,6 +1,10 @@
+// VARIABLES ------------------------------------------------------------------------------------------------------------------------- 
+
 const instagram = document.getElementById("insta");
 const flickr = document.getElementById("flickr");
 const github = document.getElementById("github"); 
+
+// Social Media ----------------------------------------------------------------------------------------------------------------------
 
 instagram?.addEventListener("click", () => {
     window.open("https://www.instagram.com/cyberlions8521/");
@@ -14,6 +18,8 @@ github?.addEventListener("click", () => {
     window.open("https://github.com/CyberLions-8521");
 });
 
+// NAVIAGTION -------------------------------------------------------------------------------------------------------------------------
+
 const cyberlions_header_logo = document.querySelector("#cyberlogoImg");
 const cyberlions_header_text = document.querySelector(".cyberlions-nav-text"); /* This is the text that says "CyberLions" */
 
@@ -23,4 +29,14 @@ cyberlions_header_logo?.addEventListener("click", () => {
 
 cyberlions_header_text?.addEventListener("click", () => {
     window.location.href = "/";
+});
+
+// MOBILE NAVIGATION ------------------------------------------------------------------------------------------------------------------
+
+const burger = document.querySelector(".mobile-menu-burger");
+
+burger?.addEventListener("click", () => {
+    const mobile_navbar = document.querySelector(".mobile-navigation-lists");
+
+    mobile_navbar.classList.toggle("mobile-navigation-bar-active");
 });
