c385288a0a7a68b6be3109dd60bc242d64300d08
Larry-Larriee <lle114@student.hbuhsd.edu>
2023-03-04 00:33:06 -0800

    Finished Our Robots Page

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -45,7 +45,7 @@ export default function Contacts() {
             <iframe title="Westminster High School" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=14325%20Goldenwest%20St,%20Westminster,%20CA%2092683+(Westminster%20High%20School)&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" width="100%" height="600" frameborder="0"><a href="https://www.maps.ie/distance-area-calculator.html">distance maps</a></iframe>
 
             <div className="contacts-info-google-maps-metadata">
-            <section>
+              <section>
                 <h1>Email</h1>
                 <p>whslionsrobotics@gmail.com</p>
               </section>
@@ -56,6 +56,7 @@ export default function Contacts() {
               </section>
             </div>
           </div>
+
         </div>
       </div>
     </>
--- src/pages/OurRobots.js
+++ src/pages/OurRobots.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { Helmet } from "react-helmet"
 
+import Robot from "../helper-components/Robot.js"
+import kevilry from "../images/robots/kelv-robot.png"
+import froggers from "../images/robots/frog-robot.png"
+
 export default function OurRobots() {
   return (
     <>
@@ -8,6 +12,29 @@ export default function OurRobots() {
         <title>Our Robots | Cyberlions</title>
       </Helmet>
 
+      <div className="our-robots">
+        <div className="our-robots-hero">
+          <h1>Meet Our Gallery of Robots.</h1>
+          <p>Learn about what we build during season</p>
+        </div>
+
+        <div className="our-robots-gallery">
+          <Robot 
+            name="Season 2022 - Kevilry" 
+            image={kevilry} 
+            description="The Kevilry is the first FRC robot able to compete in a full game. Our robot can autonomously intake, store, and shoot cargo into hub targets, as well as hang from the medium bar in the endgame hangar." 
+            award="Regional Finalist" 
+          />
+
+          <Robot 
+            name="Season 2021 - Froggers" 
+            image={froggers} 
+            description="Froggers is the first FRC robot we produced in our school. Our robot has the ability to detect and autonomously move towards power cells. In addition, with its intake system, we can collect power cells and transport them around." 
+            award="Rookie Game Changer" 
+          />
+        </div>
+
+      </div>  
     </>
   )
 }
