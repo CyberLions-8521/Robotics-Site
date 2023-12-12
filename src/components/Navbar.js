8b8abcb7b59fc923ed20995368de08780bb5db11
Larry Le <lle114@student.hbuhsd.edu>
2023-12-11 17:01:08 -0800

    initial setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Link, useMatch, useResolvedPath } from 'react-router-dom'
+import logo from '../assets/cyberlions.png'
+
+export default function Navbar() {
+  return (
+    <>
+        {/* DESKTOP NAVIGATION BAR */}
+        <header className='cyberlions-desktop-header'>
+            <nav className='cyberlions-desktop-nav'>
+                <Link className='cyberlions-desktop-nav-logo' to="/">
+                    <div className="cyberlions-nav-logo-background">
+                        <img src={logo} alt='cyberlions-logo' />
+                    </div>
+                    Cyberlion 8521
+                </Link>
+                <ul className='cyberlions-desktop-nav-links'>
+                    <CustomLink to="/about">About</CustomLink>
+                    <CustomLink to="/newsletter">Newsletter</CustomLink>
+                    <CustomLink to="/our-robots">Our Robots</CustomLink>
+                    <CustomLink to="/support-us">Support Us</CustomLink>
+                    <CustomLink to="/contacts">Contacts</CustomLink>
+                </ul>
+                <span class="material-symbols-outlined">
+                    light_mode
+                </span>
+            </nav>
+        </header>
+
+        {/* MOBILE NAVIGATION BAR */}
+    </>
+
+  )
+}
+
+function CustomLink({ to, children }) {
+    const resolvedPath = useResolvedPath(to);
+    const isActive = useMatch({ path:resolvedPath.pathname, end: true})
+
+    return (
+        <li className={ isActive ? "active" : "" }>
+            <Link to={to}>
+                {children}
+            </Link>
+        </li>
+    )
+}
