4b03f32e0abb8a77d0d75a4d8a1bcbd984b7f4eb
Larry-Larriee <LarryLe704@gmail.com>
2024-01-06 17:18:35 -0800

    gallery changes

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useRef } from 'react'
 
 import Kevilry from '../assets/images/kevilry.jpg'
+import Bingus from '../assets/images/sidewaysBingus.png'
 
 export default function Gallery() {
+
+    let gallery = useRef("gallery");
+
+    let galleryLeft = () => {
+        //huh?????????
+    }
+
     return (
         <section className='gallery'>
             <div className='galleryBox'>
@@ -11,18 +20,13 @@ export default function Gallery() {
                 </div>
                 <div className='galleryBody'>
                     <div className='galleryImageContainer'>
-                        <div className='galleryArrow galleryRight' onClick={toggleGalleryLeft}></div>
-                        <img className='galleryImage' src={Kevilry} alt={"Froggers"}></img>
+                        <div className='galleryArrow galleryRight'></div>
+                        <img className='galleryImage' src={Kevilry} alt={"Froggers"} ref={gallery} onCLick={galleryLeft}></img>
                         <div className='galleryArrow galleryLeft'></div>
                     </div>
                 </div>
                 <div className='galleryCaption'>Froggers is the first FRC robot we produced in our school. Our robot has the ability to detect and autonomously move towards power cells. In addition, with its intake system, we can collect power cells and transport them around.</div>
             </div>
         </section>
-
     )
-}
-
-let toggleGalleryLeft = () => {
-    alert("hi");
 }
