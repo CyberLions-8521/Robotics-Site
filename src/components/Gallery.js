8d89dbb39cbf87fe0dd4ec5b13a5a448c5941209
hen <whshle08@gmail.com>
2024-01-11 22:08:29 -0800

    gallery image js work
    gallery works, just need the description to change
    
    i so sleepy

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useRef } from 'react'
 
-import Kevilry from '../assets/images/kevilry.jpg'
+import Kevilry from '../assets/robots/kelv.jpg'
+import Robbie from '../assets/robots/robbie.png'
+import Froggers from '../assets/robots/froggers.jpg'
 
 export default function Gallery() {
+
+    const gallery = useRef("gallery");
+    let galleryToggle = 0;
+    let galleryImages = [Robbie, Kevilry, Froggers];
+
+    const toggleGallery = (direction) => {
+        if (direction === "left") {
+            galleryToggle = galleryToggle - 1;
+        } else if (direction === "right"){
+            galleryToggle = galleryToggle + 1;
+        }
+        gallery.current.src = (galleryImages[Math.abs(galleryToggle) % galleryImages.length]);
+    }
+
     return (
         <section className='gallery'>
             <div className='galleryBox'>
@@ -11,9 +28,9 @@ export default function Gallery() {
                 </div>
                 <div className='galleryBody'>
                     <div className='galleryImageContainer'>
-                        <div className='galleryArrow galleryRight' onClick={toggleGalleryLeft}></div>
-                        <img className='galleryImage' src={Kevilry} alt={"Froggers"}></img>
-                        <div className='galleryArrow galleryLeft'></div>
+                        <div className='galleryArrow galleryLeft' onClick={() => toggleGallery("left")}></div>
+                        <img className='galleryImage' src={Kevilry} alt={"Froggers"} ref={gallery}></img>
+                        <div className='galleryArrow galleryRight' onClick={() => toggleGallery("right")}></div>
                     </div>
                 </div>
                 <div className='galleryCaption'>Froggers is the first FRC robot we produced in our school. Our robot has the ability to detect and autonomously move towards power cells. In addition, with its intake system, we can collect power cells and transport them around.</div>
@@ -21,8 +38,4 @@ export default function Gallery() {
         </section>
 
     )
-}
-
-let toggleGalleryLeft = () => {
-    alert("hi");
 }
