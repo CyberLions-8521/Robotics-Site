772834ce69448356f6038a2740f16372f7e18117
Larry Le <lle114@student.hbuhsd.edu>
2024-01-18 15:35:44 -0800

    Dimension Fixes
    There's was a lot of faulty code. Added variables and replaced some rem with viewport units where appropriate.

--- src/Home.js
+++ src/Home.js
@@ -63,11 +63,9 @@ export default function Home() {
                     <span className="image-split" />
                 </article>
             </section>
-            <div className='hero-section-divider'>
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
                     <path fill="#c73737" fill-opacity="1" d="M0,160L80,181.3C160,203,320,245,480,245.3C640,245,800,203,960,154.7C1120,107,1280,53,1360,26.7L1440,0L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
                 </svg>
-            </div>
         </div>
         <section className='section-divider'>
             <section className='section-container hidden'>
