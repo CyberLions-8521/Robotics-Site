b5e1cf53c7221a787a2e0e5e4df260e90445c90e
Larry Le <lle114@student.hbuhsd.edu>
2024-06-11 16:22:29 -0700

    newsletter finished
    see CSS for bugs

--- src/pages/Newsletter.js
+++ src/pages/Newsletter.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import { React } from 'react'
+import { InstagramEmbed } from 'react-social-media-embed';
+
+import Mascot from '../assets/images/cyberlions-mascot.png'
 
 export default function Newsletter() {
   return (
-    <div>Newsletter</div>
+    <div className='newsletter-body'>
+      <div className='newsletter-top'>
+        <div display='flex' flexDirection='column'>
+          <div className='newsletter-header'>
+            WHAT'S BEEN GOING ON?
+          </div>
+          <div className='newsletter-description'>
+            Updates on our team's progress and accomplishments.
+          </div>
+        </div>
+        <img src={Mascot} className='newsletter-mascot' alt='Cyberlions Logo'></img>
+      </div>
+      
+      <div className='newsletter-content'>
+        <div className='newsletter-drive'>
+          <div className='newsletter-subheader'>
+            Newsletter
+          </div>
+          {/* from inside google doc: file --> share --> publish to web --> embed */}
+          <iframe title='Newsletter' className='newsletter-embed' src="https://docs.google.com/document/d/e/2PACX-1vSVZCZ-ljA1I8thZG4QW2Bv1DHZJu0XlFNedCBHAbf88pKnAxK1dsXfJsjPiaWeKYCMUyNjTPZxcw2a/pub?embedded=true"></iframe>
+        </div>
+        <div className='newsletter-instagram'>
+          <div className='newsletter-subheader'>
+            Instagram (@cyberlions8521)
+          </div>
+          {/* Instagram's embed doesn't work in react for some reason */}
+          <InstagramEmbed url='https://www.instagram.com/cyberlions8521/' className='newsletter-embed'></InstagramEmbed>
+        </div>
+      </div>
+    </div>
   )
 }
