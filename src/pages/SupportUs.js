810e46ff1a042b54828f27eed5931e5471340dc1
hen43 <whshle08@gmail.com>
2024-11-18 11:09:51 -0800

    changed sponsors
    no logos yet

--- src/pages/SupportUs.js
+++ src/pages/SupportUs.js
@@ -1,16 +1,75 @@
 import React from 'react'
-import iClever from '../assets/sponsors/2024/iClever.png'
-import SignImaging from '../assets/sponsors/2024/Sign-Imaging.png'
-import WestCoastCivil from '../assets/sponsors/2024/West-Coast-Civil.png'
-import WowLawGroup from '../assets/sponsors/2024/wow-law-group-2.png'
-import SugoiStuff from '../assets/sponsors/2023/Sugoi-Stuff.png'
-import TJAerospace from '../assets/sponsors/2023/TJ-Aero-3.png'
-import Sunmerry from '../assets/sponsors/2023/Sunmerry.png'
-import VoSandwiches from '../assets/sponsors/2023/Vo-Sandwich.png'
-import MorStar from '../assets/sponsors/2023/MorStar.png'
-import DynamicFab from '../assets/sponsors/2023/Dynamic-Fab.png'
-import Arden from '../assets/sponsors/2023/Arden2.png'
-import HeimBearings from '../assets/sponsors/2023/Heim-Bearings.png'
+// import iClever from '../assets/sponsors/2024/iClever.png'
+// import SignImaging from '../assets/sponsors/2024/Sign-Imaging.png'
+// import WestCoastCivil from '../assets/sponsors/2024/West-Coast-Civil.png'
+// import WowLawGroup from '../assets/sponsors/2024/wow-law-group-2.png'
+// import SugoiStuff from '../assets/sponsors/2023/Sugoi-Stuff.png'
+// import TJAerospace from '../assets/sponsors/2023/TJ-Aero-3.png'
+// import Sunmerry from '../assets/sponsors/2023/Sunmerry.png'
+// import VoSandwiches from '../assets/sponsors/2023/Vo-Sandwich.png'
+// import MorStar from '../assets/sponsors/2023/MorStar.png'
+// import DynamicFab from '../assets/sponsors/2023/Dynamic-Fab.png'
+// import Arden from '../assets/sponsors/2023/Arden2.png'
+// import HeimBearings from '../assets/sponsors/2023/Heim-Bearings.png'
+
+const goldSponsors = [
+  'WOW Law Group',
+];
+
+const silverSponsors = [
+  'None yet'
+];
+
+const bronzeSponsors = [
+  'Little Hill Design',
+  'Plato\'s Closet',
+  'West Coast Civil',
+  'Harbor Freight Tools',
+];
+
+const productDonations = [
+  'Hot Topic',
+  'Sign Imaging',
+  'California Fish Grill',
+  'Sensitive Sweets',
+  'Urban Plates',
+  'Core Digital Media',
+  'Ohuhu',
+  'Milk Jar Cookies',
+  'Crown Gold Exchange',
+  'TAP Electronics',
+  'Phillips Steel',
+]
+
+const listGoldSponsors = goldSponsors.map(sponsor =>
+  <li>{sponsor}</li>
+);
+
+const listSilverSponsors = silverSponsors.map(sponsor =>
+  <li>{sponsor}</li>
+);
+
+const listBronzeSponsors = bronzeSponsors.map(sponsor =>
+  <li>{sponsor}</li>
+);
+
+const listProductSponsors = productDonations.map(sponsor =>
+  <li>{sponsor}</li>
+);
+
+/* at some point make these a list and function with key and value properties.
+  this would also allow them to have logos.
+
+e.g.
+
+id: 0,
+name:'WOW Law',
+sponsorType:'Gold',
+logo:{LOGO},
+
+
+*/
+
 
 export default function SupportUs() {
   return (
@@ -39,23 +98,19 @@ export default function SupportUs() {
 
       <div className='support-sponsors'>
         <h1 className='support-header'>HUGE THANK YOU TO OUR CURRENT SPONSORS</h1>
-        <div className='support-sponsor-images'>
-          <img alt='iClever' src={iClever} className='sponsor-img'/>
-          <img alt='Sign Imaging' src={SignImaging} className='sponsor-img'/>
-          <img alt='West Coast Civil' src={WestCoastCivil} className='sponsor-img'/>
-          <img alt='WOW Law Group' src={WowLawGroup} className='sponsor-img'/>
-          <img alt='Sugoi Stuff' src={SugoiStuff} className='sponsor-img'/>
-          <img alt='TJ Aerospace' src={TJAerospace} className='sponsor-img'/>
-          <img alt='Sunmerry' src={Sunmerry} className='sponsor-img'/>
-          <img alt='Vo Sandwiches' src={VoSandwiches} className='sponsor-img'/>
-          <img alt='MorStar' src={MorStar} className='sponsor-img'/>
-          <img alt='Dynamic Fab' src={DynamicFab} className='sponsor-img'/>
-          <img alt='Arden' src={Arden} className='sponsor-img'/>
-          <img alt='Heim Bearings' src={HeimBearings} className='sponsor-img'/>
-        </div>
+          <div className='support-sponsor-text' >
+            <h2 className='support-tier'>Golden Lions</h2>
+            <ul className='support-bullets'>{listGoldSponsors}</ul>
+            <h2 className='support-tier'>Silver Lions</h2>
+            <ul className='support-bullets'>{listSilverSponsors}</ul>
+            <h2 className='support-tier'>Bronze Lions</h2>
+            <ul className='support-bullets'>{listBronzeSponsors}</ul>
+            <h2 className='support-tier'>Product Donations</h2>
+            <ul className='support-bullets'>{listProductSponsors}</ul>
+          </div>
         <p className='support-subheader'>CONTACT US AT WHSLIONSROBOTICS@GMAIL.COM</p>
         <p className='support-subheader'>
-          WE ALSO HAVE A <a href="https://gofund.me/96ab06ee" target="_blank" rel="noopener noreferrer" class="support-link">GOFUNDME</a> :)
+          WE ALSO HAVE A <a href="https://gofund.me/96ab06ee" target="_blank" rel="noopener noreferrer" class="support-link">GOFUNDME</a>
           </p>
       </div>
     </div>
