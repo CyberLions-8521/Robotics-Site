c9265b9e021d0e847caa688c3d0f28bcc5913222
RubyD <143292762+ohnoruu@users.noreply.github.com>
2025-08-29 18:35:31 +0000

    Deploy site to docs folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { React, useRef, useEffect } from 'react'
-import { Route, Routes, useLocation } from 'react-router-dom'
+import { Route, Routes, useLocation, Link } from 'react-router-dom'
 import './App.css'
 
 import Home from './pages/Home'
@@ -42,10 +42,10 @@ export default function App() {
                 </section>
 
                 <ul>
-                <li><a href='/about'>About</a></li>
-                <li><a href='/newsletter'>Newsletter</a></li>
-                <li><a href='/our-robots'>Our Robots</a></li>
-                <li><a href='/support-us'>Support Us</a></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/newsletter">Newsletter</Link></li>
+                    <li><Link to="/our-robots">Our Robots</Link></li>
+                    <li><Link to="/support-us">Support Us</Link></li>
                 </ul>
             </nav>
         </div>
