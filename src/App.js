87056af9df215ec299303097ab4c0a4e9c03ba21
ohnoruu <143292762+ohnoruu@users.noreply.github.com>
2025-09-01 19:16:03 -0700

    Debug GitHub Pages navigation error

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import SquareLogo from './assets/images/square-logo.png';
 export default function App() {
 
     function openHome() {
-        window.location.href = '/';
+        window.location.href = process.env.PUBLIC_URL || '/';
     }
 
     let location = useLocation();
